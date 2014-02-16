1d288865620e4d21acd113dd8f235033daf2a9c2
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-16 17:12:14 -0500

    added commandline arguments

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var fs = require('fs'),
 	_ = require('underscore'),
+	cwd = process.cwd(),
 	templates = require('./bin/templates.js'),
-	target = process.cwd() + '/output/',
-	optimist = require('optimist');
+	argv = require('optimist').argv,
+	target = argv._[0] || './output/',
+	jerryfile = argv.jerry || 'jerry.json',
+	data;
+console.log(cwd + '/' + jerryfile);
+if (!fs.existsSync(cwd + '/' + jerryfile)) {
+	console.error("No jerry.json file supplied or existing in this directory.");
+	return;
+}
+
+data = JSON.parse(fs.readFileSync(jerryfile));
 
 if (!fs.exists(target)) {
 	console.log('Making Directory');
 	fs.mkdirSync(target);
 }
 console.log('Compiling');
-fs.writeFileSync(target + 'foo.txt', templates.package({}));
+fs.writeFileSync(target + 'foo.txt', templates.package(data));
 console.log('Done');
